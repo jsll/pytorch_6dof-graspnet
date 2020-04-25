14215756d0555e042451fb3c894399e68ea5175e
Jens Lundell <jens.lundell@aalto.fi>
2020-04-25 13:52:27 +0300

    Fixed script to download pretrained models

--- checkpoints/download_models.sh
+++ checkpoints/download_models.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:906bfd0873094ddb5680093a42256beb955766ba3ac1ca6e62b51f319fa2e214
-size 259
+oid sha256:613aaf7171883d5abc8dbaba93eaeb3a33477bfb742d90e90b7bbf5331d0f368
+size 342
